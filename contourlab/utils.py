025fa622885301882435414b21a7ff4a0155d531
Elham Zakeri <lhmzakeri@gmail.com>
2025-08-29 14:05:20 -0400

    Added make_grouped_mean to utils.py

--- contourlab/utils.py
+++ contourlab/utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.interpolate import griddata
 import pandas as pd
-from typing import List
+from typing import List, Union, Optional, Dict
 
 
 # -----------------------------------------------------------------------------
@@ -17,7 +17,7 @@ def interpolate_grid(
     mask = np.isfinite(X) & np.isfinite(Y) & np.isfinite(Z)
     if mask.sum() < 3:
         return X, Y, np.ma.masked_invalid(Z)
-    
+
     x_flat, y_flat, z_flat = X.flatten(), Y.flatten(), Z.flatten()
     mask_flat = mask.flatten()
     # --- Build finer grid -----------------------------------------------------
@@ -102,3 +102,82 @@ def filter_high_values(
         .reset_index(drop=True)
     )
     return out
+
+
+# -----------------------------------------------------------------------------
+def label_dataframe(
+    dataframe: pd.DataFrame, col: str, thr: Union[int, float, str] = 1000
+) -> pd.DataFrame:
+    """
+    Binary labeling of a datframe based on a threshold applied to a column assigns
+    1 if dataframe[col] == thr, else 0.
+    """
+    dataframe["label"] = np.where(dataframe[col] == thr, 1, 0)
+    return dataframe
+
+
+# -----------------------------------------------------------------------------
+def make_grouped_mean(
+    datadir: Union[str, List[str]],
+    filters: Optional[Dict[str, Union[int, float, str]]] = None,
+    target_col: str = "label",
+    drop_cols: Optional[List[str]] = None,
+    first_group_keys: Optional[List[str]] = None,
+    second_group_keys: Optional[List[str]] = None,
+    do_label: bool = False,
+    label_col: Optional[str] = None,
+    label_thr: Union[int, float, str] = 1000,
+) -> pd.DataFrame:
+    """
+    2-stage grouping with optional filtering
+
+    Args:
+    datadir: str or List
+        Path(s) to datafile(s)
+    filters: dict, optional
+        Dictionary of column: value filters to apply before grouping.
+        Example:{"period": 110, "temperature": 37}
+    target_col : str, default = "label"
+        Column to average
+    drop_cols: list of str, optional
+        Drop unwanted columns.
+    first_group_keys : list of str, optional
+        Grouping keys for the first aggregation step.
+    second_group_keys: list of str, optional
+        Keys for the second aggregation step
+    do_label : bool
+        create binary labels  (Y/N: 1/0)
+    label_col: str, optional
+    label_thr: int, float, str
+        reference for binary labeling
+
+
+    returns:
+    pd.DataFrame
+        Aggregated DataFrame with column "mean_<target_col>"
+    """
+    # --- Load Data ---------------------------------------------------------------
+    if isinstance(datadir, str):
+        df = [pd.read_csv(datadir, sep=r"\s+")]
+    else:
+        df = [pd.read_csv(f, sep=r"\s+") for f in datadir]
+    df = pd.concat(df, ignore_index=True)
+    # --- Drop unwanted cols ------------------------------------------------------
+    if drop_cols:
+        df.drop(columns=drop_cols, errors="ignore", inplace=True)
+    # --- Apply labeling ----------------------------------------------------------
+    if do_label:
+        if label_col is None:
+            raise ValueError("You must specify 'label_col' when do_label=True")
+        df = label_dataframe(df, col=label_col, thr=label_thr)
+    # --- Apply filters -----------------------------------------------------------
+    if filters:
+        for col, val in filters.items():
+            df = df[df[col] == val]
+    # --- Fine grouping -----------------------------------------------------------
+    if first_group_keys:
+        df = df.groupby(first_group_keys, as_index=False).agg({target_col: "mean"})
+    # --- Coarser grouping --------------------------------------------------------
+    if second_group_keys:
+        df = df.groupby(second_group_keys, as_index=False).agg({target_col: "mean"})
+    return df 
